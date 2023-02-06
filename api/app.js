db5c3093419e26f69f3f3a587951d875fe6ccb40
mssoheil <mssoheil88@yahoo.com>
2023-02-06 17:11:07 +0330

    ci: init

---
+++ api/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json([
+        {
+            "id": "2a",
+            "title": "Book Review"
+        }
+    ])
+});
+
+app.listen(4000, () => {
+    console.log("listening on port 4000.........");
+});
